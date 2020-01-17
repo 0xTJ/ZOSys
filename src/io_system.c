f7934edf70f2f1ead3bb3fe1268b1cf8f6be6650
Thomas Jager <thomasj@thomasjager.ca>
2020-01-17 16:07:13 -0500

    Bandaid for files not building

--- src/io_system.c
+++ src/io_system.c
@@ -1,8 +1,9 @@
 #include "io_system.h"
+// #include <arch/scz180.h>
 
 mutex_t io_system_mtx;
 
 void io_system_init(void) {
-    io_system = IO_SYSTEM_PASSIVE;
+    // io_system = IO_SYSTEM_PASSIVE;
     mutex_init(&io_system_mtx);
 }
